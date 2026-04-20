8f26f3f06ad83c8d0b2de2dab807bd3fc5730d4c
Cee <69067657+CeeeeeN@users.noreply.github.com>
2026-04-20 21:52:53 +0800

    Add files via upload
    fix: firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, browserLocalPersistence, setPersistence, enableIndexedDbPersistence } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth, browserLocalPersistence, setPersistence } from "firebase/auth";
+import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager  } from "firebase/firestore";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
@@ -15,24 +15,16 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-// Enable Offline Persistence
-enableIndexedDbPersistence(db)
-  .catch((err) => {
-    if (err.code === 'failed-precondition') {
-      // This happens if the user has multiple tabs of your app open at once.
-      console.warn("Multiple tabs open, persistence disabled.");
-    } else if (err.code === 'unimplemented') {
-      // The user is on a very old browser.
-      console.warn("Browser does not support offline persistence.");
-    }
-  });
-
-export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const messaging = getMessaging(app);
-
 setPersistence(auth, browserLocalPersistence);
 
+export const messaging = getMessaging(app);
 
+
+export const db = initializeFirestore(app, {
+  localCache: persistentLocalCache({
+    tabManager: persistentMultipleTabManager()
+  })
+});
 
 export default app;
